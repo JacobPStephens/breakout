9e55d6d77d8fe4e5c74ffcc54d006c999bf315b1
JacobPStephens <jacobpstephens9@gmail.com>
2025-08-27 17:39:37 -0400

    first commit

---
+++ Game.h
@@ -0,0 +1,32 @@
+#ifndef GAME_CLASS_H
+#define GAME_CLASS_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+class Game {
+
+    public:
+
+        GameState State;
+        unsigned int Width, Height;
+        bool Keys[1024];
+        Game(unsigned int width, unsigned int height);
+
+        // initialize shaders, textures, levels
+        void Init();
+
+        // core game loop
+        void ProcessInput(float dt);
+        void Update(float dt);
+        void Render();
+
+};
+
+#endif
